22f198f92afbe7566c4801eb7327bc7ed524d2a4
Gengur123 <meleearthur@gmail.com>
2021-06-11 16:47:39 -0700

    Removed a form line and polished code a bit

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,15 @@
 
 function numberConverter(numberInput, nameInput) {
   array = []
-  userName = nameInput
-  console.log(userName)
   for (let index = 0; index <= numberInput; index += 1) {
     let indexToString = index + ""
     array.push(indexToString)
     
   }
   const robotArray = array.map(function (string) {
+    userName = nameInput
     if (string.includes("3") === true) {
-      return string = "Won't you be my neighbor?" + userName;
+      return string = "Won't you be my neighbor?";
     } else if (string.includes("2") === true) {
       return string = "Boop!";
     } else if (string.includes("1") === true) {
@@ -30,11 +29,8 @@ $(document).ready(function () {
   $("form#number-converter").submit(function (event) {
     event.preventDefault();
     const numberInput = $("input#number").val();
-    const nameInput = $("input#name").val();
     const robotLang = numberConverter(numberInput)
-    console.log(nameInput)
-    numberConverter(numberInput,nameInput);
-    console.log(nameInput)
+    numberConverter(numberInput);
     $("#output").html(robotLang);
   });
 });
