b05d9d67f806b39cffb0a1ff2091875c5d846ee1
Gengur123 <meleearthur@gmail.com>
2021-06-11 15:44:48 -0700

    Added a second form tag asking for users name

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 //Business
 
-function numberConverter(numberInput) {
+function numberConverter(numberInput, nameInput) {
   array = []
+  userName = nameInput
+  console.log(userName)
   for (let index = 0; index <= numberInput; index += 1) {
     let indexToString = index + ""
     array.push(indexToString)
+    
   }
   const robotArray = array.map(function (string) {
     if (string.includes("3") === true) {
-      return string = "Won't you be my neighbor?";
+      return string = "Won't you be my neighbor?" + userName;
     } else if (string.includes("2") === true) {
       return string = "Boop!";
     } else if (string.includes("1") === true) {
@@ -27,8 +30,11 @@ $(document).ready(function () {
   $("form#number-converter").submit(function (event) {
     event.preventDefault();
     const numberInput = $("input#number").val();
+    const nameInput = $("input#name").val();
     const robotLang = numberConverter(numberInput)
-    numberConverter(numberInput);
+    console.log(nameInput)
+    numberConverter(numberInput,nameInput);
+    console.log(nameInput)
     $("#output").html(robotLang);
   });
 });
