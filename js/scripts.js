3764ac1fb27c89ad8254806809ec71d9256eed10
Gengur123 <meleearthur@gmail.com>
2021-06-11 13:45:21 -0700

    Removed console logs and added some CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,29 @@
 
 function numberConverter(numberInput) {
   array = []
-  for (let index =0; index <=numberInput; index +=1){
+  for (let index = 0; index <= numberInput; index += 1) {
     let indexToString = index + ""
     array.push(indexToString)
+  }
+  const robotArray = array.map(function (string) {
+    if (string.includes("3") === true) {
+      return string = "Won't you be my neighbor?";
+    } else if (string.includes("2") === true) {
+      return string = "Boop!";
+    } else if (string.includes("1") === true) {
+      return string = "Beep!";
+    } else {
+      return string;
     }
-    console.log(array)
-    const robotArray = array.map (function(string) {
-      if  (string.includes("3") === true) {
-        return string = "Won't you be my neighbor?";
-      } else if (string.includes("2") === true) {
-        return string = "Boop!";
-      } else if (string.includes("1") === true) {
-        return string = "Beep!";
-      } else {
-        return string;
-      }
-    });
-    console.log(robotArray)
-    robotString = robotArray.join(" ");
-    console.log(robotString)
-    return robotString
+  });
+  robotString = robotArray.join(" ");
+  return robotString
 }
 
 //UI Logic
 
-$(document).ready(function(){
-  $("form#number-converter").submit(function(event){
+$(document).ready(function () {
+  $("form#number-converter").submit(function (event) {
     event.preventDefault();
     const numberInput = $("input#number").val();
     const robotLang = numberConverter(numberInput)
